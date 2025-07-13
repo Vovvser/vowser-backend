ac7115f3d5aa70d843e71427d15e267092bba200
SinnoLn <sintmtn69@gmail.com>
2025-07-13 13:56:39 +0900

    Chore: 필요한 의존성 추가
    - 웹소켓, FeignClient, Cache

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,9 +29,12 @@ dependencies {
     // ── Runtime ───────────────────────────────────────────────
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
 
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
 
