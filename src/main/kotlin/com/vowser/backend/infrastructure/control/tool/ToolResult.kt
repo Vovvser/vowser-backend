d2c7c3c16c6e91e558094110aede268eb8706961
SinnoLn <sintmtn69@gmail.com>
2025-09-05 02:14:59 +0900

    Remove: Kotlin 파일 삭제

--- src/main/kotlin/com/vowser/backend/infrastructure/control/tool/ToolResult.kt
+++
@@ -1,6 +0,0 @@
-package com.vowser.backend.infrastructure.control.tool
-
-data class ToolResult (
-    val message: String,
-    val isSuccess: Boolean = true
-)
--- src/test/kotlin/com/vowser/backend/TestApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.vowser.backend
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-
-fun main(args: Array<String>) {
-	fromApplication<Application>().with(TestcontainersConfiguration::class).run(*args)
-}
